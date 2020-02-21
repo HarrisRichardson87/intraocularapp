d2ed592d3ed701f70c0c5c96a7bc5bbc27624d85
Harris Richardson <harris.richardson87@gmail.com>
2020-02-21 11:47:26 +0100

    Updates

---
+++ precache-manifest.893e77b4dbc337e79b6adc8166c05ca7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae54e3e6d475a29ae5a0398a776afe5",
+    "url": "/intraocularapp/index.html"
+  },
+  {
+    "revision": "85f4e4beaf7f3c89bb0c",
+    "url": "/intraocularapp/static/css/2.cf33d70f.chunk.css"
+  },
+  {
+    "revision": "d688ccae7a0f6ea82ce6",
+    "url": "/intraocularapp/static/css/main.c0c113b9.chunk.css"
+  },
+  {
+    "revision": "85f4e4beaf7f3c89bb0c",
+    "url": "/intraocularapp/static/js/2.c11eb103.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/intraocularapp/static/js/2.c11eb103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d688ccae7a0f6ea82ce6",
+    "url": "/intraocularapp/static/js/main.099e2978.chunk.js"
+  },
+  {
+    "revision": "5f0a167f885e92cfb56c",
+    "url": "/intraocularapp/static/js/runtime-main.a074f211.js"
+  },
+  {
+    "revision": "3288dc89c6a398fdb4b8b8fa2426a267",
+    "url": "/intraocularapp/static/media/Premier-League.3288dc89.png"
+  },
+  {
+    "revision": "5c573269fc24ec679f4352a9700cfaf3",
+    "url": "/intraocularapp/static/media/eye.5c573269.png"
+  },
+  {
+    "revision": "b78d06b2287dfbc29814535cdaf8a445",
+    "url": "/intraocularapp/static/media/harryCartoon.b78d06b2.png"
+  },
+  {
+    "revision": "16527fdc4c4493c9bb19b6b086767be7",
+    "url": "/intraocularapp/static/media/mocartoon.16527fdc.png"
+  }
+]);
